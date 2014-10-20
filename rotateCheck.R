e5ebf4b1499324f0b80a19d3dce483545578fd0f
OganM <ogan.mancarcii@gmail.com>
2014-10-19 18:24:12 -0700

    bug in rotateCheck fixed that opened an unnecesary folder

--- rotateCheck.R
+++ rotateCheck.R
@@ -15,7 +15,7 @@ rotateCheck = function(rotationOut){
     
     # in server version full.names input of list.dirs do not work. This fixes it. Might add this to ogbox as an overwrite.
     loopAround = gsub(paste0(dirFols[1],'/'),'',loopAround)
-
+    loopAround = loopAround[!grepl(dirFols[1],loopAround)]
     
     loopAround = loopAround [-which(loopAround %in% c('Relax','Marker',''))]
     dir.create(paste0(rotationOut,'/Confidence'), showWarnings = F)
