3d744972cb5de4638149bc56da2941d50bf1def7
OganM <ogan.mancarcii@gmail.com>
2014-10-21 17:48:45 -0700

    Slightly better homology matching. only slightly...

--- metaphOrthology.R
+++ metaphOrthology.R
@@ -2,45 +2,6 @@ library(biomaRt)
 
 metaphorDir = 'metaphors/phylomedb.org/metaphors'
 metaphorPrep = function(metaphorDir){
-    # matching metaphor IDs to gene symbols -----
-    system(paste0('gunzip ',metaphorDir,'/latest/','id_conversion.txt.gz'))
-    
-    # isolate the mouse and human ones and just take swissport IDs
-    idConvert = read.table(paste0(metaphorDir,'/latest/','id_conversion.txt'),header=T,sep = '\t',quote='"',comment.char = '')
-    idConvert = idConvert[grepl(paste0('_','((',mouseCode,')|(',humanCode,'))$'),
-                                idConvert$protid),]
-    idConvert = idConvert[idConvert$db =='swissprot',]
-    
-    
-    system(paste0('gzip ',metaphorDir,'/latest/','id_conversion.txt &'))
-    
-    # get gene translations
-    humanMart = useMart("ensembl", dataset="hsapiens_gene_ensembl")
-    humanTrans = getBM(attributes = c('uniprot_swissprot','hgnc_symbol'),
-                       # just take the human ones. just in case...
-                       filters = 'uniprot_swissprot', 
-                       values = idConvert$X.extid[grep(humanCode,idConvert$protid)],
-                       mart = humanMart)
-    names(humanTrans) = c('X.extid', 'symbol')
-    
-    mouseMart = useMart("ensembl", dataset="mmusculus_gene_ensembl")
-    
-    
-    mouseTrans = getBM(attributes = c('uniprot_swissprot','mgi_symbol'),
-                       # just take the human ones. just in case...
-                       filters = 'uniprot_swissprot', 
-                       values = idConvert$X.extid[grep(mouseCode,idConvert$protid)],
-                       mart = mouseMart)
-    names(mouseTrans) = c('X.extid','symbol')
-    
-    allTrans = rbind(mouseTrans,humanTrans)
-    
-    mouseTranslate = merge(idConvert, mouseTrans)
-    humanTranslate = merge(idConvert, humanTrans)
-    symbolSwiss =  merge(idConvert,allTrans)
-    
-    
-    
     # locate mouse and human files -----
     system(paste0('gunzip ',metaphorDir,'/latest/','species.txt.gz'))
     species = read.table(paste0(metaphorDir,'/latest/','species.txt'),header=T,sep = '\t',quote='"')
@@ -50,6 +11,66 @@ metaphorPrep = function(metaphorDir){
     mouseCode = species[species[3] =='Mus musculus',2]
     mouse = species[species[3] =='Mus musculus',1]
     humanCode = species[species[3] =='Homo sapiens',2]
+    
+    
+    # matching metaphor IDs to gene symbols -----
+    system(paste0('gunzip ',metaphorDir,'/latest/','id_conversion.txt.gz'))
+    
+    # isolate the mouse and human ones and just take swissport IDs
+    idConvert = read.table(paste0(metaphorDir,'/latest/','id_conversion.txt'),header=T,sep = '\t',quote='"',comment.char = '')
+    idConvert = idConvert[grepl(paste0('_','((',mouseCode,')|(',humanCode,'))$'),
+                                idConvert$protid),]
+    idConvert = idConvert[idConvert$db %in% c('swissprot','trembl'),]
+
+    
+    system(paste0('gzip ',metaphorDir,'/latest/','id_conversion.txt &'))
+    
+    # get gene translations
+    humanMart = useMart("ensembl", dataset="hsapiens_gene_ensembl")
+    humanTrans = getBM(attributes = c('uniprot_swissprot','hgnc_symbol'),
+                       # just take the human ones. just in case...
+                       filters = 'uniprot_swissprot',
+                       values = idConvert$X.extid,
+                       mart = humanMart)
+    humanTrans2 = getBM(attributes = c('uniprot_sptrembl','hgnc_symbol'),
+                        # just take the human ones. just in case...
+                        filters = 'uniprot_sptrembl',
+                        values = idConvert$X.extid,
+                        mart = humanMart)
+    
+
+
+    names(humanTrans) = c('X.extid', 'symbol')
+    names(humanTrans2) = c('X.extid', 'symbol')
+    
+    humanTrans=rbind(humanTrans,humanTrans2)
+    
+    mouseMart = useMart("ensembl", dataset="mmusculus_gene_ensembl")
+    
+    
+    mouseTrans = getBM(attributes = c('uniprot_swissprot','mgi_symbol'),
+                       # just take the human ones. just in case...
+                       filters = 'uniprot_swissprot', 
+                       values = idConvert$X.extid,
+                       mart = mouseMart)
+    mouseTrans2 = getBM(attributes = c('uniprot_sptrembl','mgi_symbol'),
+                       # just take the human ones. just in case...
+                       filters = 'uniprot_sptrembl', 
+                       values = idConvert$X.extid,
+                       mart = mouseMart)
+    names(mouseTrans) = c('X.extid','symbol')
+    names(mouseTrans2) = c('X.extid','symbol')
+    mouseTrans = rbind(mouseTrans,mouseTrans2)
+    
+    allTrans = rbind(mouseTrans,humanTrans)
+    
+    mouseTranslate = merge(idConvert, mouseTrans)
+    humanTranslate = merge(idConvert, humanTrans)
+    symbolSwiss =  merge(idConvert,allTrans)
+    
+    
+    
+  
     
     
     # get human orthologues ------
@@ -66,7 +87,7 @@ metaphorPrep = function(metaphorDir){
     humanOrthos = merge(humanOrthos, humanTranslate[,3:4])
     names(mouseTranslate)[c(3,4)] = c('protid2', 'mouseGene')
     humanOrthos = merge(humanOrthos, mouseTranslate[,3:4])
-    write.table(humanOrthos[,c('humanGene','mouseGene','CS')], file = 'Data/homology/humanMouse',quote= F
+    write.table(unique(humanOrthos[,c('humanGene','mouseGene','CS')]), file = 'Data/homology/humanMouse',quote= F
                 ,row.names=F,
                 sep = '\t')
     
@@ -80,7 +101,7 @@ metaphorPrep = function(metaphorDir){
     mouseOrthos = merge(mouseOrthos, mouseTranslate[,3:4])
     names(humanTranslate)[c(3,4)] = c('protid2', 'humanGene')
     mouseOrthos = merge(mouseOrthos, humanTranslate[,3:4])
-    write.table(humanOrthos[,c('mouseGene','humanGene','CS')], file = 'Data/homology/mouseHuman',quote= F
+    write.table(unique(mouseOrthos[,c('mouseGene','humanGene','CS')]), file = 'Data/homology/mouseHuman',quote= F
                 ,row.names=F,
                 sep = '\t')
     
