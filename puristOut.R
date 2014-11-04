2d10827292d739e80fa030853ac0a298594d0dea
OganM <ogan.mancarcii@gmail.com>
2014-11-03 17:22:16 -0800

    puristOut modified to support lilah's method

--- puristOut.R
+++ puristOut.R
@@ -3,16 +3,23 @@ eval( expr = parse( text = getURL(
     "https://raw.githubusercontent.com/oganm/toSource/master/ogbox.r",
     ssl.verifypeer=FALSE) ))
 
-puristOut = function(geneLoc){
+puristOut = function(geneLoc, lilah = F){
     filenames = list.files(geneLoc,include.dirs = FALSE)
     fileContents = lapply(paste0(geneLoc,'/', filenames), read.table)
     geneList = vector(mode = 'list', length = length(fileContents))
     names(geneList) = filenames
-    if (ncol(fileContents[[1]])==3){
+    
+    if (ncol(fileContents[[1]])==3 & lilah == F){
         # this if for a combination of fold change and silhouette coefficient
         for (i in 1:length(fileContents)){
             geneList[[i]] = as.character(fileContents[[i]]$V1[as.numeric(as.character(fileContents[[i]]$V3))>0.5
                                                               & as.numeric(as.character(fileContents[[i]]$V2))>log(10,base=2)])
+        }
+    }else if (ncol(fileContents[[1]])==3 & lilah == T){
+        # this if for lilah's selection method
+        for (i in 1:length(fileContents)){
+            geneList[[i]] = as.character(fileContents[[i]]$V1[as.numeric(as.character(fileContents[[i]]$V3))*
+                                                              as.numeric(as.character(fileContents[[i]]$V2))>2])
         }
     } else if (ncol(fileContents[[1]])==1){
         # this is for a mere gene list
