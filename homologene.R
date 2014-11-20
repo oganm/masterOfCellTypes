0c85a3d829a45af36e7a20a557b95f20f125d6e5
OganM <ogan.mancarcii@gmail.com>
2014-11-19 17:58:27 -0800

    homologene,system commands replaced, target directory slightly softened

--- homologene.R
+++ homologene.R
@@ -1,6 +1,8 @@
+homoloGeneTarget = 'Data/homologene.tsv'
+
 setUpHomologene = function(){
-    download.file(url = "ftp://ftp.ncbi.nih.gov/pub/HomoloGene/current/homologene.data", destfile = 'Data/homologene.data')
-    homologene = read.table('Data/homologene.data',sep ='\t',quote='')
+    download.file(url = "ftp://ftp.ncbi.nih.gov/pub/HomoloGene/current/homologene.data", destfile = 'homologene.data')
+    homologene = read.table('homologene.data',sep ='\t',quote='')
     names(homologene) = c('HID','Taxonomy','Gene.ID','Gene.Symbol','Protein.GI','Protein.Accession')
     homoHuman = subset(homologene, Taxonomy == 9606)
     homoHuman = homoHuman[,c('HID','Gene.Symbol')]
@@ -11,18 +13,18 @@ setUpHomologene = function(){
     names(homoMouse)[2]='mouseGene'
     homoFile = merge(homoHuman,homoMouse)
     rm('homoHuman','homoMouse')
-    system('rm Data/homologene.data')
-    write.table(homoFile,file='Data/homologene.tsv', quote=F,sep = '\t',col.names=T,row.names=F)
+    file.remove('homologene.data')
+    write.table(homoFile,file=homoloGeneTarget, quote=F,sep = '\t',col.names=T,row.names=F)
 }
 
 
 mouse2human = function(genes){
-    homolo = read.table('Data/homologene.tsv',header=T,sep='\t')
+    homolo = read.table(homoloGeneTarget,header=T,sep='\t')
     homolo = homolo[homolo$mouseGene %in% genes,]
     return(homolo[,c('mouseGene','humanGene')])
 }
 human2mouse = function(genes){
-    homolo = read.table('Data/homologene.tsv',header=T,sep='\t')
+    homolo = read.table(homoloGeneTarget,header=T,sep='\t')
     homolo = homolo[homolo$humanGene %in% genes,]
     return(homolo[,c('humanGene','mouseGene')])
 }
