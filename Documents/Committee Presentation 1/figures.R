f691201a641859166568ecaf314e57c6b22354de
OganM <ogan.mancarcii@gmail.com>
2014-12-23 16:47:40 -0800

    Rest of the presentation files. They are a little messy but reproducible

--- Documents/Committee Presentation 1/figures.R
+++ Documents/Committee Presentation 1/figures.R
@@ -1,3 +1,6 @@
+
+# simulated coexpression plot ---------
+setwd('Documents/Committee Presentation 1')
 require(ggplot2)
 red = c(3,3,2)
 blue= c(4,2,5)
@@ -26,7 +29,7 @@ svg('4.coexpression.svg')
 
 dev.off()
 
-
+# simulated expression change plot before -------------------
 
 fakeGenes = data.frame(gene = factor(paste0('Gene',1:7)),
                        expression =  runif(7,6,16) , 
@@ -48,7 +51,7 @@ svg('1.exprBefore.svg')
 
 dev.off()
 
-
+# simulated expression change after -------------------
 svg('2.exprAfter.svg')
 
 fakeGenes$expression[fakeGenes$color=='red'] = fakeGenes$expression[fakeGenes$color=='red'] / 4
@@ -63,3 +66,79 @@ fakeGenes$expression[fakeGenes$color=='red'] = fakeGenes$expression[fakeGenes$co
 )
 
 dev.off()
+
+# human data degredation plot ------
+# code looks weird because it's taken from qc file. that whole list business
+# is not necessary
+require(RCurl)
+eval( expr = parse( text = getURL(
+    "https://raw.githubusercontent.com/oganm/toSource/master/ogbox.R",
+    ssl.verifypeer=FALSE) ))
+parent = getParent(2)
+source('runVars.R')
+
+setwd(parent)
+
+
+
+humanDesign = read.table('../../Data/humanCellTypeDesign.tsv',header=T,sep='\t', stringsAsFactors=F)
+
+
+gsms = humanDesign[,1]
+
+gsms = strsplit(gsms,',')
+
+
+platforms = unique(humanDesign$Platform)
+
+require(affy)
+
+affies = lapply( rep("AffyBatch", len(platforms)), new )
+celsInFolder = list.files('../../humanCellTypeCel')
+celsNoExtension = gsub('[.](C|c)(E|e)(L|l)','',celsInFolder)
+
+for (i in 1){
+    toRead = unlist(gsms[humanDesign$Platform %in% platforms[i]])
+    relevant = celsInFolder[celsNoExtension %in% toRead]
+    affies[i] = ReadAffy(filenames = paste0('../../humanCellTypeCel/',relevant))
+}
+
+names(affies) = platforms
+
+
+
+setwd('../..')
+svg('5.exprAfter.svg')
+
+plotAffyRNAdeg(affies[[1]])
+dev.off()
+
+# not working markers plot ----------------
+load(bipolLoc)
+bpCntScz = aned_high_GSE12649
+bpCnt = aned_high_GSE5388
+bpCntSczDes = Samples_GSE12649
+bpCntDes = Samples_GSE5388
+rm(aned_high_GSE12649)
+rm(aned_high_GSE5388)
+rm(Samples_GSE12649)
+rm(Samples_GSE5388)
+
+windowSize = 4
+frame = data.frame(t(bpCnt[c('NOV','FAM131A'),4:ncol(bpCnt)]),
+           group = gsub('_.*','',names(bpCnt[4:ncol(bpCnt)])))
+
+frame = melt(frame)
+svg("6. not working gene expression")
+(ggplot(frame, aes(x= variable, y = value))
+ + geom_point()  
+ + theme(axis.text.x  = element_text(vjust=windowSize/2, size=20),
+         axis.title.y = element_text(vjust=0.5, size=20),
+         axis.title.x = element_text(vjust=0.5, size=0) ,
+         title = element_text(vjust=0.5, size=20),
+         strip.text.x = element_text(size = 20),
+         axis.text.y = element_text(size = 16))
+ +  scale_y_continuous(name="log2 Expression"))
+
+def.off()
+
