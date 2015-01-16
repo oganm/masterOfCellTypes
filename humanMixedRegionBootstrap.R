5f7c9ea120a00bd1c326539a4024ea565bac0431
OganM <ogan.mancarcii@gmail.com>
2015-01-15 17:35:12 -0800

    humanMixedRegionBootstrap: shorthened to use readHumanCel

--- humanMixedRegionBootstrap.R
+++ humanMixedRegionBootstrap.R
@@ -1,7 +1,8 @@
 require(RCurl)
 eval( expr = parse( text = getURL(
-    "https://raw.githubusercontent.com/oganm/toSource/master/ogbox.r",
+    "https://raw.githubusercontent.com/oganm/toSource/master/ogbox.R",
     ssl.verifypeer=FALSE) ))
+source('readHumanCel.R')
 
 #humanMixedRegionBootstrap = function(humanSoftLoc,hCelDir, hBootOut)
 
@@ -12,7 +13,7 @@ hCelDir = 'humanRegionCel'
 humanRegions = read.table('Data/hugeHumanSoft.tsv', header = T, sep = '\t', stringsAsFactors = F)
 
 humanRegions = humanRegions[humanRegions$platform=='GPL5175'
-                            & !humanRegions$Disease=='Cancer',]
+                            & !humanRegions$CDeath=='Cancer',]
 
 
 require(foreach)
@@ -33,44 +34,15 @@ dir.create(hBootOut,showWarnings = F, recursive = T)
 
 size = 15
 # foreach(i = 2:100) %dopar% {
-for (i in 1:500){
+for (i in 474:500){
     allRegions = unique(humanRegions$Region)
     subsetInd = vector(length = len(allRegions)*size)
     for (j in 1:len(allRegions)){
         subsetInd[(j*size+1-size):(j*size)] = sample(which(humanRegions$Region %in% allRegions[j]),size)
     }
     subsetRegions = humanRegions[subsetInd,]
-    relevantCels = sapply(subsetRegions$GSM, function(x){which(grepl(x,cels))})
-    sampleCels = cels[relevantCels]
-    affyRaw = read.celfiles(paste0(hCelDir,'/',sampleCels))
-    exonTS <- rma(affyRaw, target = "core")
-    rm(affyRaw)
-    featureData(exonTS) <- getNetAffx(exonTS, "transcript")
-    #View the features of the obtained data
-    # exonTS
-    #Extract the expression data
-    exp_value <- get("exprs", pos=assayData(exonTS))
+    readHumanCel(subsetRegions$GSM,paste0('Data/HumanBootstrap/',i),humanDir='humanRegionCel')
     
-    #Extract the proset ids
-    PS_id <- pData(featureData(exonTS))[, "probesetid"]
-    
-    #Extract transcript annotations
-    PS_an <- t(sapply(pData(featureData(exonTS))[, "geneassignment"], function(x) {
-        annotation <- strsplit(x, " // ")[[1]][3]
-        gene_symbol <- gsub(" ", "",strsplit( x, " // ")[[1]][2])
-        mir <- paste(grep("MIR", strsplit(x, " // ")[[1]], value = T), collapse = " ")
-        c(annotation, gene_symbol, mir)
-    }))
-    
-    rownames(PS_an) <- PS_id
-    colnames(PS_an) <-c("Annotation", "Gene_Symbol", "mRNAs")
-    
-    
-    #Create the expression file
-    aned <- merge(PS_an, exp_value, by.x="row.names", by.y="row.names", all.x=TRUE, sort=FALSE)
-    rownames(aned) <- aned[,1]
-    aned <- aned[,-1]
-    write.csv(aned, paste0(hBootOut,'/',i), row.names=FALSE)
 }
 
 
