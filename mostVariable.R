a63e24b6c13c0261789079837cc99e57080e1891
OganM <ogan.mancarcii@gmail.com>
2015-02-02 13:44:31 -0800

    mostVariable works with gemma, also can give table output

--- mostVariable.R
+++ mostVariable.R
@@ -1,12 +1,11 @@
 # this version merges cell types into a single sample then looks for a variable.
 # specific to our cell type data. not ideal but oh well...
-mostVariableCT = function(whichFile,outFile,selectionNaming){
+mostVariableCT = function(whichFile,outFile=NULL,selectionNaming, design='Data/meltedDesign.tsv'){
 
     allDataPre = read.csv(whichFile, header = T)
-    design = read.design('Data/meltedDesign.tsv')
-
-
-    exprData = allDataPre[,4:ncol(allDataPre)]
+    design = read.design(design)
+    
+    list[,exprData]= sepExpr(allDataPre)
     
     cellTypes = trimNAs(unique(design[,selectionNaming]))
     
@@ -14,15 +13,16 @@ mostVariableCT = function(whichFile,outFile,selectionNaming){
         apply(exprData[,design[,selectionNaming] %in% x,drop=F],1,mean)
     })
     exprData = as.data.frame(cellTypeExpr)
-
+    # remove the ones with highest expression below 6
     rowmax = apply(exprData, 1, max)
     discludeGenes = (rowmax<6)
     allDataPre = allDataPre[!discludeGenes,]
     exprData = exprData[!discludeGenes,]
     
     # ignore multiple matching probesets while mostVariable selection
-    allDataMulti = allDataPre[grepl('[|]',exprData$Gene.Symbol),]
-    allDataPre = allDataPre[!grepl('[|]',exprData$Gene.Symbol),]
+    allDataMulti = allDataPre[grepl('[|]',allDataPre$Gene.Symbol),]
+    exprData = exprData[!grepl('[|]',allDataPre$Gene.Symbol),]
+    allDataPre = allDataPre[!grepl('[|]',allDataPre$Gene.Symbol),]
     
     # you bloody idiot... taken from lila
     decreasingVar = order(apply(exprData,1,var), decreasing = T)
@@ -38,7 +38,7 @@ mostVariableCT = function(whichFile,outFile,selectionNaming){
 # this function is a generic function that looks for the most variable probeset
 # of a gene. unlike the previous one, it takes in objects and outputs objects 
 mostVariable = function(allDataPre,genes = 'Gene.Symbol'){
-    exprData = allDataPre[,4:ncol(allDataPre)]
+    list[,exprData]= sepExpr(allDataPre)
     rowmax = apply(exprData, 1, max)
     discludeGenes = (rowmax<6)
     allDataPre = allDataPre[!discludeGenes,]
