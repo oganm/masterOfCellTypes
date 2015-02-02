a115c81607d1852101de05062e78dde335ca3a5e
OganM <ogan.mancarcii@gmail.com>
2015-02-02 11:37:54 -0800

    readDesign.. changed to support a single chip. closes #54, and kinda closes #48 since we have a function for that

--- readDesignMergeCel.R
+++ readDesignMergeCel.R
@@ -2,7 +2,7 @@
 #essential parts of the file are
 #1. the first collumn must include the GSMs
 #2. platform must be written to the Platform collumn
-
+require(stringr)
 require(RCurl)
 eval( expr = parse( text = getURL(
     "https://raw.githubusercontent.com/oganm/toSource/master/ogbox.R",
@@ -12,20 +12,42 @@ sourceGithub(oganm,toSource,gemmaAnnotate)
 sourceGithub(oganm,toSource,mergeChips)
 
 
-readDesignMergeCel = function (desFile, namingCol, celRegex, celDir,tinyChip, outFolder){
-    #always have gsms in the first collumn
-
-   
-    #eval( expr = parse( text = getURL(
-    #    "https://raw.githubusercontent.com/oganm/toSource/master/mergeChips.R",
-    #    ssl.verifypeer=FALSE) ))
-    #only works in windows
-    #source('https://raw.githubusercontent.com/oganm/toSource/master/ogbox.r')
-    #source('https://raw.githubusercontent.com/oganm/toSource/master/mergeChips.R')
+readMouseCel = function(GSMs,mouseDir='cel',file=NA){
     require(affy)
     require(compare)
+    allCells = affy::list.celfiles(mouseDir, recursive = T)
+    files = sapply(GSMs,function(x){grep(paste0(x,'[.](C|c)(E|e)(L|l)$'),allCells,value=T)})
+    platforms = unique(dirname(unlist(files)))
+    affies =  lapply( rep("AffyBatch", len(platforms)), new )
+    
+    for (i in 1:len(affies)){
+        affies[i] = ReadAffy(filenames = paste0('cel/',grep(platforms[i],files, value=T)))
+    }
+    
+    if (len(affies)>1){newNormalized = mergeChips(affies[[1]],affies[[2]])
+    } else {newNormalized = rma(affies[[1]])}
+    
+    if('GPL339' %in% platforms){
+        aned = gemmaAnnot(newNormalized, 'Data/GemmaAnnots/GPL339')
+    } else{
+        aned = gemmaAnnot(newNormalized, 'Data/GemmaAnnots/GPL1261')
+    }
+    aned = aned[!aned$Gene.Symbol == '',]
+    
+    names(aned) = gsub('[.](C|c)(E|e)(L|l)','',names(aned))
+    
+    if (!is.na(file)){
+        write.csv(aned, file, row.names=FALSE)
+    }
+    invisible(aned)
+    
+}
 
+readDesignMergeCel = function (desFile, namingCol, celRegex, celDir,tinyChip, expFile,desOut){
+    #always have gsms in the first collumn
 
+    require(affy)
+    require(compare)
 
     design = read.table(desFile,quote='',header=T,sep='\t')
 
@@ -37,43 +59,17 @@ readDesignMergeCel = function (desFile, namingCol, celRegex, celDir,tinyChip, ou
         print((1:len(gsms))[lapply(gsms,len)==0])
     }
 
-    platforms = unique(design$Platform)
-
-    affies = lapply( rep("AffyBatch", len(platforms)), new )
-    names(affies) = platforms
-
-    #create a vector of affy objects based on how many chips are there just works with 2 right now
-    #implementation of a third generation is straightforward. in fact i could have done it instead
-    #of writing this but oh well...
-    for (i in 1:len(affies)){
-        celsInFolder = list.files(
-            paste0(celDir,'/',platforms[i]))
-        celsNoExtension = gsub('[.](C|c)(E|e)(L|l)','',celsInFolder)
-        gsms = regmatches(design[design$Platform == platforms[i], 1], gregexpr(celRegex, design[design$Platform == platforms[i], 1],perl=T))
-        gsms = unlist(gsms)
-
-        relevant = celsInFolder[celsNoExtension %in% gsms]
-
-        affies[i] = ReadAffy(filenames = paste0(celDir,'/',platforms[i],'/',relevant))
-    }
-
-    newNormalized = mergeChips(affies[[1]],affies[[2]])
-    
-    aned = gemmaAnnot(newNormalized, 'Data/GPL339Annotation')
-    
-    aned = aned[!aned$Gene.Symbol == '',]
-    
-    
-
-    header = gsub('.cel', '', gsub('.CEL','', colnames(aned)[4:ncol(aned)]))
-    colnames(aned) = c(colnames(aned)[1:3], header)
+    aned = readMouseCel(unlist(gsms),celDir)
+        
     dir.create(outFolder, recursive = T,showWarnings=F)
-    write.csv(aned, paste0(outFolder,"/rmaExp.csv"), row.names=FALSE)
+    
+    list[genes,exp] = sepExpr(aned)
     #boxplot(aned[,4:ncol(aned)])
     gsms = regmatches(design[, 1], gregexpr(celRegex, design[, 1],perl=T))
 
     #gsms = regmatches(df[, 1], gregexpr("(GSM\\d\\d\\d\\d\\d(\\d|))|(PC\\d....)|(Y+.*?((?=(,))|\\d+))|(((?<=:)|(?<=,))A\\d.*?30A)|(v2_(?![G,H,r]).*?((?=(,))|($)))|(SSC.*?((?=(,))|($)))|(MCx.*?((?=(,))|($)))|(Cbx.*?((?=(,))|($)))", df[, 1],perl=T))
 
+    header = names(exp)
 
     indexes = vector()
     for (i in 1:length(header)){
@@ -91,14 +87,24 @@ readDesignMergeCel = function (desFile, namingCol, celRegex, celDir,tinyChip, ou
     #newDesign$age = as.numeric(newDesign$age)
     newDesign = newDesign[order(as.numeric(rownames(newDesign))),]
 
-    write.table(newDesign, paste0(outFolder,"/meltedDesign.tsv"), row.names=FALSE,sep = '\t', quote=F)
+    write.table(newDesign, desOut, row.names=FALSE,sep = '\t', quote=F)
+    
+    #list[genes,expr]=sepExpr(aned)
+    
+    #expr = expr[match(make.names(design$sampleName)]
+    
+    design[match(colnames(exprData),make.names(design$sampleName),),]
+    
+    write.csv(aned, expFile, row.names=FALSE)
+    
 }
 
 
 # for changes in original design file that only involves naming. do not run the whole thing again
 meltDesign = function(desFile, namingCol, celRegex, exprFile, outFile){
     expr = read.csv(exprFile , header = T)
-    header =  colnames(expr)[4:ncol(expr)]
+    list[gene,expres] = sepExpr(expr)
+    header =  colnames(expres)
 
     design = read.table(desFile,quote='',header=T,sep='\t')
     design = design[!is.na(design[,namingCol]),]
