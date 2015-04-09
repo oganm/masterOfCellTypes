bb460330e85bc4df2ce3a76f85d34595ece90cb0
OganM <ogan.mancarcii@gmail.com>
2015-04-08 22:00:39 -0700

    microglialException changed to use stimulation and old age related genes

--- microglialException.R
+++ microglialException.R
@@ -1,22 +1,29 @@
-microglialException = function(restDir,cores = 1){
+require(RCurl)
+eval( expr = parse( text = getURL(
+    "https://raw.githubusercontent.com/oganm/toSource/master/ogbox.R",
+    ssl.verifypeer=FALSE) ))
+sourceGithub(OganM,toSource,'GOAD/goadDifGenes')
+
+microglialException = function(restDir, updateList = F, cores = 1){
     #applies to all files inside a directory recursively
     require(foreach)
     require(doMC)
-    require(parallel)
-    # so that I wont fry my laptop
+    require(parallel)# so that I wont fry my laptop
     if (detectCores()<cores){ 
         cores = detectCores()
         print('max cores exceeded')
         print(paste('set core no to',cores))
     }
     registerDoMC(cores)
+    # genes effected by old age and LPS stimulation
+    effectedGenes = unlist(goadDifGenes(ids=c(22,23),p=0.05,foldChange='ALL'))
+    effectedGenes = effectedGenes[!duplicated(effectedGenes)]
     
     fileNames = list.files(restDir, recursive =T )
-    microGenes =  unlist(read.table('Data/microgliaList.txt'))
     fileNames = fileNames[grepl('Microglia',fileNames)]
     foreach (i = fileNames) %dopar% {
         micro = read.table(paste0(restDir,'/',i))
-        micro = micro[micro$V1 %in% microGenes,]
+        micro = micro[!micro$V1 %in% effectedGenes,]
         write.table(micro, quote = F, row.names = F, col.names = F, paste0(restDir,'/',i))
     }
 }
